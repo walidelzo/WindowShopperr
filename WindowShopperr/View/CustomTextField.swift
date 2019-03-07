bfc6e897e33c6ea6a21dbe1a842375ec5981f9fa
Admin <walidelzo@gmail.com>
2019-03-07 10:33:33 +0200

    add some futears to app

--- WindowShopperr/View/CustomTextField.swift
+++ WindowShopperr/View/CustomTextField.swift
@@ -10,6 +10,24 @@ import UIKit
 @IBDesignable
 class CustomTextField: UITextField {
 
+    override func draw(_ rect: CGRect) {
+        let size:CGFloat = 20
+        let currencyLBL=UILabel(frame: CGRect(x: 5, y: (frame.size.height/2)-size/2, width: 20, height: 20))
+        currencyLBL.backgroundColor=#colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
+        currencyLBL.clipsToBounds=true
+        currencyLBL.layer.cornerRadius=3.0
+        currencyLBL.textColor=#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        currencyLBL.textAlignment = .center
+       
+        let formmatter = NumberFormatter()
+        formmatter.numberStyle = .currency
+        formmatter.locale = .current
+        currencyLBL.text=formmatter.currencySymbol
+        addSubview(currencyLBL)
+        
+    }
+    
+    
     override func prepareForInterfaceBuilder() {
         CustomizeText()
     }
@@ -24,11 +42,11 @@ class CustomTextField: UITextField {
         backgroundColor=#colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
         layer.cornerRadius=5.0
         
-//      if let place=placeholder {
-//            let placeAttr = NSAttributedString(string: place, attributes: [.foregroundColor : #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+//     if let place=placeholder {
+//        let placeAttr = NSAttributedString(string: place, attributes: [.foregroundColor : #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)])
 //      attributedText=placeAttr
 //        }
-        font=UIFont.systemFont(ofSize: 13)
+       font=UIFont.systemFont(ofSize: 13)
         keyboardType = .decimalPad
         textAlignment = .center
         
